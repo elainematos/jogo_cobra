8c3ffdee17c6459de10072980135e26142f36970
Elaine Matos <nany.sara@hotmail.com>
2020-11-15 23:39:22 -0300

    Definir direções e impedir que a cobrinha suma

--- js/script.js
+++ js/script.js
@@ -16,13 +16,27 @@ function criarBG() {
 
 function criarCobrinha() {
 	for (i=0; i < cobra.length; i++) {
-		context.fillStyle ="green";
+		context.fillStyle = "green";
 		context.fillRect(cobra[i].x, cobra[i].y, box, box);
 	}
 }
 
+document.addEventListener("keydown", update);
+
+function update (event) {
+	if(event.keyCode == 37 && direcao != "right") direcao = "left";
+	if(event.keyCode == 38 && direcao != "down") direcao = "up";
+	if(event.keyCode == 39 && direcao != "left") direcao = "right";
+	if(event.keyCode == 40 && direcao != "up") direcao = "down";
+}
+
 function iniciarJogo() {
-	criarBG();
+	if(cobra[0].x > 15 * box && direcao == "right") cobra[0].x = 0;
+	if(cobra[0].x < 0 && direcao == "left") cobra[0].x = 16 * box;
+	if(cobra[0].y > 15 * box && direcao == "down") cobra[0].y = 0;
+	if(cobra[0].y < 0 && direcao == "up") cobra[0].y = 16 * box;
+
+	criarBG(); 
 	criarCobrinha();
 
 	let cobraX = cobra[0].x;
